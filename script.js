69bc17f3f724dc70ab6f832d96c70de649dc92cd
Raphael <naduma.raphael4@gmail.com>
2025-01-02 11:59:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,34 +18,3 @@ solve.onclick = function(){
 clear.onclick = function(){
     display.value = "";
 };
-
-/*
-const writeBtns = document.getElementsByClassName("write");
-const display = document.getElementById("display");
-const clear = document.getElementById("CLEAR");
-const solve = document.getElementById("SOLVE");
-
-for (let btn of writeBtns){
-    btn.addEventListener("click", clickWrite);
-};
-
-
-function clickWrite(){
-    const writeValue = this.textContent;
-    display.value += writeValue;
-};
-
-function keyWrite(){
-    
-};
-
-solve.onclick = function(){
-    display.value = eval(display.value);
-};
-
-clear.onclick = function(){
-    display.value = "";
-};
-
-window.addEventListener("keydown", keyWrite);
-*/
