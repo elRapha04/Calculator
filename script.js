c633ea2f162c1a95dd461d57a5fe0e3c589bdf2d
Raphael <naduma.raphael4@gmail.com>
2025-01-03 12:59:50 +0800

    initial

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const writeBtns = document.getElementsByClassName("write");
-const display = document.getElementById("display");
+// const writeBtns = document.getElementsByClassName("write");
+// const display = document.getElementById("display");
 
-for (let btn of writeBtns){
-    btn.addEventListener("click", function(){
-        const writeValue = this.textContent;
-        display.value += writeValue;
-    });
-};
+// for (let btn of writeBtns){
+//     btn.addEventListener("click", function(){
+//         const writeValue = this.textContent;
+//         display.value += writeValue;
+//     });
+// };
 
-const clear = document.getElementById("CLEAR");
-const solve = document.getElementById("SOLVE");
+// const clear = document.getElementById("CLEAR");
+// const solve = document.getElementById("SOLVE");
 
-solve.onclick = function(){
-    display.value = eval(display.value);
-};
+// solve.onclick = function(){
+//     display.value = eval(display.value);
+// };
 
-clear.onclick = function(){
-    display.value = "";
-};
+// clear.onclick = function(){
+//     display.value = "";
+// };
 
-let dark = true;
+// let dark = true;
 
-function toggleMode(){
-    if (dark){
-        document.body.style.backgroundColor = "white";
-        dark = false;
-    }
-    else{
-        document.body.style.backgroundColor = "tomato";
-        dark = true;
-    }
-}
+// function toggleMode(){
+//     if (dark){
+//         document.body.style.backgroundColor = "white";
+//         dark = false;
+//     }
+//     else{
+//         document.body.style.backgroundColor = "tomato";
+//         dark = true;
+//     }
+// }
 
-const wkwk = false;
