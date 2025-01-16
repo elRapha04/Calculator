8c771aa118cbb330b01efe13f4545a5519d8e855
Raphael <naduma.raphael4@gmail.com>
2025-01-16 14:14:56 +0800

    Update Main.java

--- PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
+++ PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("I am wkwk\n");
-        System.out.print("you are wkwk");
+        System.out.println("This is the FIRST output");
+        System.out.print("and this is the SECOND line.\n");
+
+        //TIP: "sout" + Tab = System.out.println();
+
+        System.out.println("Now this line was made from a shortcut");
     }
 }
