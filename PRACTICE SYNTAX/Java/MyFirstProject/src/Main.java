1fa1ee96ac8a78aeacffb34ac7a098882b213898
Raphael <naduma.raphael4@gmail.com>
2025-01-16 17:19:05 +0800

    user input

--- PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
+++ PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        /* PRINTING
         System.out.println("This is the FIRST output");
         System.out.print("and this is the SECOND line.\n");
 
         //TIP: "sout" + Tab = System.out.println();
 
         System.out.println("Now this line was made from a shortcut");
+         */
+
+        /*
+        //READING USER INPUT (requires line 1)
+        Scanner scanner = new Scanner(System.in);
+
+        //get age (number)
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt(); //reads the IMMEDIATE next integer
+
+        //get name (string)
+        System.out.print("Enter your name: ");
+        String name = scanner.next(); //reads the IMMEDIATE next string
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+
+        OTHER SCANNER METHODS:
+            nextLine() //reads the entire line including spaces
+            nextInt() //reads the next IMMEDIATE integer
+            nextDouble() //reads the next IMMEDIATE double
+            nextBoolean() //reads the next IMMEDIATE boolean
+
+        scanner.close();
+        */
+
+        
     }
 }
