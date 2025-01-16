053fa7cf250ce4e7db27b8ac837a14d1c297e818
Raphael <naduma.raphael4@gmail.com>
2025-01-16 23:49:56 +0800

    Math methods

--- PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
+++ PRACTICE SYNTAX/Java/MyFirstProject/src/Main.java
@@ -45,6 +45,39 @@ public class Main {
         System.out.println(number);
 */
 
+/*
+        //MATH EXERCISES
+
+        //Hypotenuse: c² = Math.sqrt(a² + b²)
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c;
+
+        System.out.print("Enter value of a: ");
+        a = scanner.nextDouble();
+        System.out.print("Enter value of b: ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println("The hypotenuse (side c) is: " + c);
+
+        //Get circumference, area, and volume of a circle/sphere
+        double radius, circumference, area, volume;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius; // 2πr
+        area = Math.PI * Math.pow(radius, 2); // πr²
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3); // (4/3)πr³
+
+        System.out.printf("The circumference is %.1fcm\n", circumference);
+        System.out.printf("The area is %.1fcm²\n", area);
+        System.out.printf("The volume is %.1fcm³\n", volume);
+*/
+
+        System.out.println(4 / 3);
+        System.out.println(4 / 3.0);
 
     }
 }
