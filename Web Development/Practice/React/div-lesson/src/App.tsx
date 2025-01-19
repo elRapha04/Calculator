3b739d06d048b7bde3f72a04ad1901ecc48ceae4
Raphael <naduma.raphael4@gmail.com>
2025-01-19 23:44:46 +0800

    Update App.tsx

--- Web Development/Practice/React/div-lesson/src/App.tsx
+++ Web Development/Practice/React/div-lesson/src/App.tsx
@@ -3,36 +3,36 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="border-black border-2 h-screen flex flex-col items-center">
+      <div className="border-black border-2 w-full h-screen flex flex-col justify-center items-center">
         <h1 className="">
           Beginner Programming Activity: Build a Creative Grid with React and
           Tailwind CSS!
         </h1>
 
-        <div id="grid-container" className="size-4/5 border-2 border-black space-y-2">
-          <div id="group-div" className="flex flex-row size-full">
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+        <div id="grid-container" className="size-4/5 border-2 border-red-600 space-y-2">
+          <div id="group-div" className="border-2 border-yellow-300 flex size-full">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row 1
             </div>
           </div>
 
-          <div id="group-div" className="flex flex-row size-full space-x-2">
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+          <div id="group-div" className="border-2 border-yellow-300 flex size-full space-x-2">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row 2
             </div>
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row 2
             </div>
           </div>
 
-          <div id="group-div" className="flex flex-row size-full space-x-2">
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+          <div id="group-div" className="border-2 border-yellow-300 flex size-full space-x-2">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row3
             </div>
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row3
             </div>
-            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+            <div id="inDiv" className="w-full border-2 border-green-500 flex justify-center items-center">
               Row3
             </div>
           </div>
