6c81058471cf594bcea2f0e4e3f34f37bdbc001e
Raphael <naduma.raphael4@gmail.com>
2025-01-19 22:26:56 +0800

    dffsa

--- Web Development/Practice/React/div-lesson/src/App.tsx
+++ Web Development/Practice/React/div-lesson/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="text-5xl">
+        Beginner Programming Activity: Build a Creative Grid with React and
+        Tailwind CSS!
+      </h1>
 
-export default App
+      <div id="grid-container" className="size-3 border-4 border-black">
+        <div className="group-div">
+          <div className="inDIV"></div>
+        </div>
+        <div className="group-div">
+          <div className="inDIV"></div>
+        </div>
+        <div className="group-div">
+          <div className="inDIV"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
