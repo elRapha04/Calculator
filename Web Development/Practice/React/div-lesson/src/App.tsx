cb3cd3cb6f0f049299844e6c6e965169d181ed29
Raphael <naduma.raphael4@gmail.com>
2025-01-19 23:28:56 +0800

    structured but overflowing

--- Web Development/Practice/React/div-lesson/src/App.tsx
+++ Web Development/Practice/React/div-lesson/src/App.tsx
@@ -3,21 +3,41 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="">
-        Beginner Programming Activity: Build a Creative Grid with React and
-        Tailwind CSS!
-      </h1>
-      <div id="grid-container" className="size-3 border-4 border-black">
-        <div className="group-div">
-          <div className="inDIV"></div>
+      <div className="border-black border-2 h-screen flex flex-col items-center">
+        <h1 className="">
+          Beginner Programming Activity: Build a Creative Grid with React and
+          Tailwind CSS!
+        </h1>
+
+        <div id="grid-container" className="size-4/5 border-2 border-black space-y-2">
+          <div id="group-div" className="flex flex-row size-full">
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row 1
+            </div>
+          </div>
+
+          <div id="group-div" className="flex flex-row size-full space-x-2">
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row 2
+            </div>
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row 2
+            </div>
+          </div>
+
+          <div id="group-div" className="flex flex-row size-full space-x-2">
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row3
+            </div>
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row3
+            </div>
+            <div id="inDiv" className="w-full border border-black flex justify-center items-center">
+              Row3
+            </div>
+          </div>
         </div>
-        <div className="group-div">
-          <div className="inDIV"></div>
-        </div>
-        <div className="group-div">
-          <div className="inDIV"></div>
-        </div>
-      </div>{" "}
+      </div>
     </>
   );
 }
