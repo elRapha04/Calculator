37f791eea938e33f27c2d1f8b481684954bc2c8b
Raphael <naduma.raphael4@gmail.com>
2025-01-19 22:27:01 +0800

    Revert "dffsa"
    This reverts commit 6c81058471cf594bcea2f0e4e3f34f37bdbc001e.

--- Web Development/Practice/React/div-lesson/src/App.tsx
+++ Web Development/Practice/React/div-lesson/src/App.tsx
@@ -1,26 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl">
-        Beginner Programming Activity: Build a Creative Grid with React and
-        Tailwind CSS!
-      </h1>
+    <div>App</div>
+  )
+}
 
-      <div id="grid-container" className="size-3 border-4 border-black">
-        <div className="group-div">
-          <div className="inDIV"></div>
-        </div>
-        <div className="group-div">
-          <div className="inDIV"></div>
-        </div>
-        <div className="group-div">
-          <div className="inDIV"></div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export default App
