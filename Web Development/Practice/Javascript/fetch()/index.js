4cbe3f605ffbebc9d9931e072ba909dc19ca0b2a
Raphael <naduma.raphael4@gmail.com>
2025-01-21 14:14:41 +0800

    FINISHED

--- Web Development/Practice/Javascript/fetch()/index.js
+++ Web Development/Practice/Javascript/fetch()/index.js
@@ -1,30 +1,33 @@
 fetchData();
 
-function fetchData(){
-  const input = document.getElementById("input");
+function fetchData() {
+  const input = document.getElementById("input").value.toLowerCase();
   const submitBtn = document.getElementById("submitBtn");
+
   const output = document.getElementById("output");
+  const image = document.getElementById("image");
   const name = document.getElementById("name");
   const height = document.getElementById("height");
   const weight = document.getElementById("weight");
   const id = document.getElementById("id");
-  
-  fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then(response => {
-    
-    if(!response.ok){
-      throw new Error("Couldn't find resource");
-    }
-    
-    return response.json();
-    
-  })
-  .then(data => {
-    name.textContent = `Name: ${data.forms[0].name}`;
-    height.textContent = `Height: ${data.height}`;
-    weight.textContent = `Weight: ${data.height}`;
-    id.textContent = `ID: ${data.id}`;
-    
-  })
-  .catch(error => console.error(error))
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Couldn't find resource");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      image.src = data.sprites.front_default;
+      output.style.display = "block";
+      
+      const upperName = data.forms[0].name;
+      name.textContent = `Name: ${upperName.charAt(0).toUpperCase()+upperName.slice(1)}`;
+      height.textContent = `Height: ${data.height}`;
+      weight.textContent = `Weight: ${data.weight}`;
+      id.textContent = `ID: ${data.id}`;
+    })
+    .catch((error) => console.error(error));
 }
