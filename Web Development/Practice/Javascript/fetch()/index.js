e7edf7ea3f9b4252f95deef4ce9d3c874dee87c5
Raphael <naduma.raphael4@gmail.com>
2025-01-21 13:52:13 +0800

    display

--- Web Development/Practice/Javascript/fetch()/index.js
+++ Web Development/Practice/Javascript/fetch()/index.js
@@ -1,12 +1,15 @@
-// fetchData();
+fetchData();
 
-// function fetchData(){
-//   const input = document.getElementById("input");
-//   const submitBtn = document.getElementById("submitBtn");
-//   const output = document.getElementById("output");
-// }
-
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+function fetchData(){
+  const input = document.getElementById("input");
+  const submitBtn = document.getElementById("submitBtn");
+  const output = document.getElementById("output");
+  const name = document.getElementById("name");
+  const height = document.getElementById("height");
+  const weight = document.getElementById("weight");
+  const id = document.getElementById("id");
+  
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto")
   .then(response => {
     
     if(!response.ok){
@@ -14,7 +17,14 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto")
     }
     
     return response.json();
-
+    
   })
-  .then(data => console.log(data.forms[0].name))
+  .then(data => {
+    name.textContent = `Name: ${data.forms[0].name}`;
+    height.textContent = `Height: ${data.height}`;
+    weight.textContent = `Weight: ${data.height}`;
+    id.textContent = `ID: ${data.id}`;
+    
+  })
   .catch(error => console.error(error))
+}
