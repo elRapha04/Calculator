7dc40f24139f59728bc4fd267f98323f9d97fc72
Raphael <naduma.raphael4@gmail.com>
2025-01-21 13:27:43 +0800

    Update index.js

--- Web Development/Practice/Javascript/fetch()/index.js
+++ Web Development/Practice/Javascript/fetch()/index.js
@@ -1,12 +1,20 @@
-function fetchData(){
-  const input = document.getElementById("input");
-  const submitBtn = document.getElementById("submitBtn");
-  const output = document.getElementById("output");
-  
-  try {
+// fetchData();
+
+// function fetchData(){
+//   const input = document.getElementById("input");
+//   const submitBtn = document.getElementById("submitBtn");
+//   const output = document.getElementById("output");
+// }
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then(response => {
     
-  }
-  catch (error){
-    console.error(error)
-  };
-}
+    if(!response.ok){
+      throw new Error("Couldn't find resource");
+    }
+    
+    return response.json();
+
+  })
+  .then(data => console.log(data.id))
+  .catch(error => console.error(error))
