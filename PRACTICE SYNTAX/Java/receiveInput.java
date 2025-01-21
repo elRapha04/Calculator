25d91020480c289f89c44bb58393b6f603880875
Raphael <naduma.raphael4@gmail.com>
2025-01-21 22:14:50 +0800

    receiveInput.java

--- PRACTICE SYNTAX/Java/receiveInput.java
+++ PRACTICE SYNTAX/Java/receiveInput.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class receiveInput {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+/*
+    SOME SCANNER METHODS:
+      nextLine() //reads the entire line including spaces
+      nextInt() //reads the next IMMEDIATE integer
+      nextDouble() //reads the next IMMEDIATE double
+      nextBoolean() //reads the next IMMEDIATE boolean
+*/
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello there, %s\n", name);
+
+        System.out.print("\nPlease enter your age: ");
+        int age = scanner.nextInt();
+        System.out.printf("I see that you are %d years old!", age);
+    }
 }
