99565a6ff505f3da7486a5a2e2b91b82deffe1ff
Raphael <naduma.raphael4@gmail.com>
2025-01-21 13:13:03 +0800

    finish html

---
+++ Web Development/Practice/Javascript/fetch()/index.js
@@ -0,0 +1,12 @@
+function fetchData(){
+  const input = document.getElementById("input");
+  const submitBtn = document.getElementById("submitBtn");
+  const output = document.getElementById("output");
+  
+  try {
+    
+  }
+  catch (error){
+    console.error(error)
+  };
+}
