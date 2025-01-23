2f7f76ac64408bce1933215f19a3b36d4b37f71f
Raphael <naduma.raphael4@gmail.com>
2025-01-23 22:19:25 +0800

    update printf

--- PRACTICE SYNTAX/Java/printf.java
+++ PRACTICE SYNTAX/Java/printf.java
@@ -14,18 +14,24 @@ public class printf {
                 boolean => b
 
             FLAGS
+                + = output a plus
+                , = comma grouping separator
+                ( = negative numbers are enclosed in ()
+                space = display a minus if negative, space if positive
 
             WIDTH
+                0 => zero padding
+                number => right-justified padding
+                negative number => left-justified padding
 
             PRECISION
-
+                .number => set decimal places
          */
 
         System.out.printf("SPECIFIER CHARACTER:\n");
         System.out.printf("\tString: %s\n", "Hello");
-        System.out.printf("\tInt: %i\n", 23);
-        System.out.printf("\tDecimal: %d\n", 456);
-        System.out.printf("\tDouble%\n", 7890);
+        System.out.printf("\tInt: %d\n", 456);
+        System.out.printf("\tDouble: %f\n", 7890);
         System.out.printf("\tBoolean: %b\n", false);
 
         System.out.printf("FLAGS:\n");
