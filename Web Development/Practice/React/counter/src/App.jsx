1b1bf6be1ac18760b5843d2a1bac6e5a94e732c6
Raphael <naduma.raphael4@gmail.com>
2025-01-28 16:33:50 +0800

    B4 BOOTSTRAP

--- Web Development/Practice/React/counter/src/App.jsx
+++ Web Development/Practice/React/counter/src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="w-full h-screen bg-zinc-700 flex flex-col justify-center items-center">
+      <h1 className="text-9xl font-bold font-mono">{count}</h1>
+
+    </div>
+  );
 };
 
 export default App;
