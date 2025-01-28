42f06f9d054f132d6716b8a34dccf62f08004f44
Raphael <naduma.raphael4@gmail.com>
2025-01-28 16:50:44 +0800

    FINISHED COUNTER

--- Web Development/Practice/React/counter/src/App.jsx
+++ Web Development/Practice/React/counter/src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
 
   function increment(){
-    
+    setCount(count + 1);
   }
   
   function reset(){
-    
+    setCount(0);
   }
   
   function decrement(){
-    
+    setCount(count - 1);
   }
 
   return (
@@ -22,19 +22,19 @@ const App = () => {
 
       <div className="w-3/4 flex justify-evenly">
         <button 
-        onClick={() => increment()}
-        className="bg-green-500 border-2 p-4 rounded-2xl cursor-pointer font-bold hover:bg-white active:scale-95">
-          INCREASE
+        onClick={decrement}
+        className="bg-red-500 border-2 p-4 rounded-2xl cursor-pointer font-bold hover:bg-white active:scale-95">
+          DECREASE
         </button>
         <button 
-        onClick={() => reset()}
+        onClick={reset}
         className="bg-blue-500 border-2 p-4 rounded-2xl cursor-pointer font-bold hover:bg-white active:scale-95">
           RESET
         </button>
         <button 
-        onClick={() => decrement()}
-        className="bg-red-500 border-2 p-4 rounded-2xl cursor-pointer font-bold hover:bg-white active:scale-95">
-          DECREASE
+        onClick={increment}
+        className="bg-green-500 border-2 p-4 rounded-2xl cursor-pointer font-bold hover:bg-white active:scale-95">
+          INCREASE
         </button>
       </div>
     </div>
