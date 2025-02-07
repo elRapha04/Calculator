0ec101393234ed148956c212fb82192567f7aebc
Raphael <naduma.raphael4@gmail.com>
2025-02-07 18:16:01 +0800

    STEP 4.5

--- Web Development/Projects/Slot Machine/project.js
+++ Web Development/Projects/Slot Machine/project.js
@@ -2,6 +2,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOL_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUE = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
 function deposit() {
   while (true) {
     const depositAmount = prompt("Enter deposit amount: ");
@@ -15,30 +32,29 @@ function deposit() {
   }
 }
 
-
 // 2. Determine number of lines to bet on
 
-function numOfLines(){
-  while (true){
+function numOfLines() {
+  while (true) {
     const lines = prompt("How many lines to bet on : ");
     const numLines = parseFloat(lines);
 
-    if(isNaN(numLines ) || numLines <= 0 || numLines > 3){
+    if (isNaN(numLines) || numLines <= 0 || numLines > 3) {
       console.log("Invalid line count, try again.");
     } else {
-      return numLines
+      return numLines;
     }
   }
 }
 
 // 3. Collect the bet amount
 
-function getBet(balance, lines){
-  while (true){
+function getBet(balance, lines) {
+  while (true) {
     const bet = prompt("Enter bet per line: ");
     const numBet = parseFloat(bet);
 
-    if (isNaN(numBet) || numBet <= 0 || balance <= (numBet * lines)){
+    if (isNaN(numBet) || numBet <= 0 || balance <= numBet * lines) {
       console.log("Invalid input, try again.");
     } else {
       return numBet;
@@ -47,13 +63,26 @@ function getBet(balance, lines){
 }
 
 // 4. Spin the slot machine
+
+function spin() {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+  console.log(symbols);
+}
+
+spin();
+
 // 5. Check if the player won
 // 6. Give the player's winnings
 // 7. Play again
 
-let balance = deposit();
-const lines = numOfLines();
-const bet = getBet(balance, lines);
-console.log(balance);
-console.log(lines);
-console.log(bet);
+// let balance = deposit();
+// const lines = numOfLines();
+// const bet = getBet(balance, lines);
+// console.log(balance);
+// console.log(lines);
+// console.log(bet);
