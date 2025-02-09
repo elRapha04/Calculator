eb5193b14fe1b1a32dc083f4599eb29e089aa830
Raphael <naduma.raphael4@gmail.com>
2025-02-09 22:17:26 +0800

    node

--- PRACTICE SYNTAX/nodeJS/server.js
+++ PRACTICE SYNTAX/nodeJS/server.js
@@ -1,12 +1,30 @@
 import http from "http";
 import dotenv from "dotenv";
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("Herro warudo");
+  
+  if (req.method === 'GET'){
+    if (req.url === '/'){
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end();
+    } 
+    else if (req.url === '/about'){
+
+    }
+  }
+  
+  
 });
 
 server.listen(PORT, () => {
