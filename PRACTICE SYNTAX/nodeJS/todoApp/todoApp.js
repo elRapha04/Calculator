2ada0657339fb58fc158f08bcb42560be504a6f3
Raphael <naduma.raphael4@gmail.com>
2025-02-10 13:32:54 +0800

    fail

--- PRACTICE SYNTAX/nodeJS/todoApp/todoApp.js
+++ PRACTICE SYNTAX/nodeJS/todoApp/todoApp.js
@@ -1,16 +1,39 @@
 import { createServer } from 'http';
 import fs from 'fs';
+import prompt from 'prompt-sync';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT;
+// ACTIONS
+const actions = {
+  1: "View tasks",
+  2: "Add a task",
+  3: "Tick a task",
+  4: "Delete a task"
+};
+
+for (const [key, value] in Object.entries(actions)){
+  console.log(`[${key}] ${value}`);
+}
+
+// CHOOSE ACTION
+// function action(){
+  
+// };
+// VIEW ALL TODOS
+
+// ADD A TODO
+// DELETE A TODO
+// MARK AS DONE
 
 // SERVER
+const PORT = process.env.PORT;
 const server = createServer((req, res) => {
+  console.log("");
+  
   fs.readFile("./todos.json", (err,  data) => {
-    res.writeHead(200, {"content-type": "text/html"});
+    res.writeHead(200, {"content-type": "text/plain"});
     if (err){
-      res.writeHead(404, {"content-type": "text/plain"});
       res.end(`Error: ${err}`);
     }
     
