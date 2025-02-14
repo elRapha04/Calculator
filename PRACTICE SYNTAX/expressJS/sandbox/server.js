3ee93ea6a87027331fc1ec58192c801752968f2d
Raphael <naduma.raphael4@gmail.com>
2025-02-14 17:23:47 +0800

    DELETE JS30

--- PRACTICE SYNTAX/expressJS/sandbox/server.js
+++ PRACTICE SYNTAX/expressJS/sandbox/server.js
@@ -4,8 +4,14 @@ require("dotenv").config({ path: "./.env" });
 
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.send('<h1>Landing Page</h1>')
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page</h1>')
+});
+
+app.listen(PORT, () => console.log(`App is running in port http://localhost:${PORT}`));
 
 
-app.listen(PORT);
-
-console.log(`App is running in port http://localhost:${PORT}`);
